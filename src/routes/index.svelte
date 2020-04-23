39e08e30c50f95fc7e6f7369648faba582d0edd8
Antony Jones <aj@desirableobjects.co.uk>
2020-04-23 16:16:21 +0100

    update app

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,7 @@
-<style>
-	
-</style>
-
 <script>
-  import { goto } from '@sapper/app'
+  import { goto, stores } from '@sapper/app'
+  
+  const { session } = stores()
   
   let email = null
   let password = 'user123'
@@ -30,12 +28,18 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
+{#if $session.authenticated}
+<p>You are logged in as {$session.profile.name}</p>
+{:else}
+<form>
 <p>Login</p>
 <select bind:value={email}>
-  <option value={null}>-- Choose --</option>
-  <option value="user@example.com">User</option>
+  <option value={null}>-- Select User Type --</option>
+  <option value="user@example.com">Regular</option>
   <option value="owner@example.org">Owner</option>
   <option value="admin@example.net">Admin</option>
 </select>
 <input type="password" bind:value={password} />
 <button type="button" disabled={!email} on:click={login}>Log in</button>
+</form>
+{/if}
